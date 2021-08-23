91d761a34cb858cf8cbfeedf0b75e75468715ef6
DNg'andu <daniel.ngandu@brhc.com>
2021-08-23 09:44:04 +0200

    2nd push

---
+++ app/Http/Livewire/PostDatatable.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+use App\Post;
+
+class PostDatatable extends Component
+{
+    use WithPagination;
+    public $searchTerm;
+    public $sortColumn = 'created_at';
+    public $sortDirection = 'asc';
+
+    private function headerConfig()
+    {
+        return [
+            'id' => '#',
+            'title' => 'Title',
+            'content' => 'Content',
+            'created_at' => [
+                'label' => 'Creation Date',
+                'func' => function($value) {
+                    return $value->diffForHumans();
+                }
+            ]
+        ];
+    }
+
+    public function sort($column)
+    {
+        $this->sortColumn = $column;
+        $this->sortDirection = $this->sortDirection == 'asc' ? 'desc' : 'asc';
+    }
+
+    private function resultData()
+    {
+        return Post::where(function ($query) {
+            $query->where('user_id', '=', auth()->user()->id);
+
+            if($this->searchTerm != "") {
+                $query->where('title', 'like', '%'.$this->searchTerm.'%');
+                $query->orWhere('content', 'like', '%'.$this->searchTerm.'%');
+            }
+        })
+        ->orderBy($this->sortColumn, $this->sortDirection)
+        ->paginate(5);
+    }
+
+    public function render()
+    {
+        return view('livewire.post-datatable', [
+            'data' => $this->resultData(),
+            'headers' => $this->headerConfig()
+        ]);
+    }
+}
